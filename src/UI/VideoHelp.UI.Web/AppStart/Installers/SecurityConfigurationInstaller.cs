a59c3c068e4eb3f7d31f0d45ee9682cd163d000e
Alexey Kurtov <alexey.kurtov@gmail.com>
2012-04-07 15:06:56 +0400

    Recreate UI.Web

---
+++ src/UI/VideoHelp.UI.Web/AppStart/Installers/SecurityConfigurationInstaller.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using FluentSecurity;
+using VideoHelp.UI.Web.Controllers;
+
+namespace VideoHelp.UI.Web.AppStart.Installers
+{
+    public class SecurityConfigurationInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            SecurityConfigurator.Configure(configuration =>
+                                               {
+                                                   configuration.ResolveServicesUsing(type => container.ResolveAll(type).OfType<object>());
+
+                                                   // Let Fluent Security know how to get the authentication status of the current user
+                                                   configuration.GetAuthenticationStatusFrom(() => UserManager.IsUserLogged);
+
+                                                   configuration.ForAllControllers().DenyAnonymousAccess();
+
+                                                   // This is where you set up the policies you want Fluent Security to enforce on your controllers and actions
+                                                   configuration.For<HomeController>(x => x.Index()).Ignore();
+  
+                                                   //configuration.For<HomeController>(x => x.Test()).DenyAuthenticatedAccess();
+
+                                                   configuration.For<AuthenticationController>().Ignore();
+                                                   configuration.For<AuthenticationController>(x => x.Logoff()).DenyAnonymousAccess();
+                                               });
+        }
+    }
+}
---
+++ src/UI/VideoHelp.UI.Web/Controllers/MeetingsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using VideoHelp.ReadModel;
+using VideoHelp.ReadModel.Meeting;
+
+namespace VideoHelp.UI.Web.Controllers
+{
+    public class MeetingsController : Controller
+    {
+        private readonly IReadRepository _repository;
+        
+        public MeetingsController(IReadRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_repository.GetAll<MeetingListView>());
+        }
+
+    }
+}
